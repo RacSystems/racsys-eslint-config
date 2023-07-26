7f95c3307efc7508def2905e7916b3b62b68e677
leonardosoares1 <leonardomsoares1@gmail.com>
2023-07-26 11:01:02 -0300

    build: v1.3.12

--- node.js
+++ node.js
@@ -20,18 +20,6 @@ module.exports = {
     'typescript-sort-keys'
   ],
   rules: {
-    'prettier/prettier': [
-      'error',
-      {
-        printWidth: 80,
-        tabWidth: 2,
-        singleQuote: true,
-        trailingComma: 'all',
-        arrowParens: 'always',
-        semi: true,
-        endOfLine: 'auto',
-      },
-    ],
     'import-helpers/order-imports': [
       'warn',
       {
