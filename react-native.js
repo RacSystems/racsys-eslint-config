62d63f524ea70eefc78b90ceb9064c504667f400
leonardosoares1 <leonardomsoares1@gmail.com>
2024-01-03 14:17:04 -0300

    feat: nova configuração para react native

--- react-native.js
+++ react-native.js
@@ -1,6 +1,5 @@
 module.exports = {
   env: {
-    browser: true,
     es2021: true
   },
   extends: [
@@ -113,19 +112,7 @@ module.exports = {
         'noSortAlphabetically': false,
         'reservedFirst': false
       }
-    ],
-    'jsx-a11y/alt-text': [
-      'warn',
-      {
-        elements: ['img'],
-        img: ['Image'],
-      },
-    ],
-    'jsx-a11y/aria-props': 'warn',
-    'jsx-a11y/aria-proptypes': 'warn',
-    'jsx-a11y/aria-unsupported-elements': 'warn',
-    'jsx-a11y/role-has-required-aria-props': 'warn',
-    'jsx-a11y/role-supports-aria-props': 'warn',
+    ]
   },
   settings: {
     react: {
